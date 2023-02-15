e25b766fd48922d2d845a796dcabe7d0bb7be040
Sunil V L <sunilvl@ventanamicro.com>
2023-02-15 10:17:07 +0530

    drivers/acpi: RISC-V: Add RHCT related code
    RHCT is a new table defined for RISC-V to communicate the
    features of the CPU to the OS. Create a new architecture folder
    in drivers/acpi and add RHCT parsing code.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>

--- arch/riscv/include/asm/acpi.h
+++ arch/riscv/include/asm/acpi.h
@@ -61,6 +61,15 @@ static inline void arch_fix_phys_package_id(int num, u32 slot) { }
 
 struct acpi_madt_rintc *acpi_cpu_get_madt_rintc(int cpu);
 u32 get_acpi_id_for_cpu(int cpu);
+int acpi_get_riscv_isa(struct acpi_table_header *table,
+		       unsigned int cpu, const char **isa);
+#else
+static inline int acpi_get_riscv_isa(struct acpi_table_header *table,
+				     unsigned int cpu, const char **isa)
+{
+	return -EINVAL;
+}
+
 #endif /* CONFIG_ACPI */
 
 #endif /*_ASM_ACPI_H*/
