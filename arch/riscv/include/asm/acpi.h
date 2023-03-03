07f3d1078576f3095b0ad8c6881ddd599da5441a
Sunil V L <sunilvl@ventanamicro.com>
2023-03-03 17:25:36 +0530

    RISC-V: smpboot: Add ACPI support in smp_setup()
    Enable SMP boot on ACPI based platforms by using the RINTC
    structures in the MADT table.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>

--- arch/riscv/include/asm/acpi.h
+++ arch/riscv/include/asm/acpi.h
@@ -63,6 +63,8 @@ struct acpi_madt_rintc *acpi_cpu_get_madt_rintc(int cpu);
 u32 get_acpi_id_for_cpu(int cpu);
 int acpi_get_riscv_isa(struct acpi_table_header *table,
 		       unsigned int cpu, const char **isa);
+
+static inline int acpi_numa_get_nid(unsigned int cpu) { return NUMA_NO_NODE; }
 #else
 static inline int acpi_get_riscv_isa(struct acpi_table_header *table,
 				     unsigned int cpu, const char **isa)
